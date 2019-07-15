eed1fa9a5d448702982dd20097d478b744962e99
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-15 04:55:29 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,6 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string :name 
       t.integer :price 
       t.string :image_url
+      t.string :size
       t.timestamps
   end 
 end 
