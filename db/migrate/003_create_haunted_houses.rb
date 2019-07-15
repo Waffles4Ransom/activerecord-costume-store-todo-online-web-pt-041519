76e2a76721a3746ce5750a796e19f72eca420947
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-15 05:14:02 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,10 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change 
     create_table :haunted_houses do |t|
       t.string :name 
-    
+      t.string :location 
+      t.string :theme
+      t.integer :price 
+      
     end 
   end 
 end 
