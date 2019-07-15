28e725511b355d2a232d6b52607014961cef4e0c
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-15 05:04:38 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,9 @@
 # Create your costume_stores migration here
-class CostumeStore
+class CostumeStores < ActiveRecord::Migration[4.2]
+  def change 
+    create_table :coustume_stores do |t|
+      t.string :name 
+      t.string :location 
+      
+  end 
+end 
